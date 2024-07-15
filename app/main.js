892a6d4998266ca93a6fffc432e3edbfd4e20044
s19835 <s19835@sci.pdn.ac.lk>
2024-07-15 19:37:42 +0530

    add space, debug

--- app/main.js
+++ app/main.js
@@ -4,22 +4,28 @@ import util from "util";
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
+// - decodeBencode("i52e") -> 52
+// - decodeBencode("l5:helloi52ee") -> ["hello", 52]
 function decodeBencode(bencodedValue) {
   // Check if the first character is a digit
   if (!isNaN(bencodedValue[0])) {
     const firstColonIndex = bencodedValue.indexOf(":");
+    
     if (firstColonIndex === -1) {
       throw new Error("Invalid encoded value");
     }
+    
     return bencodedValue.substr(firstColonIndex + 1);
   } 
 
   // Check if the first and last character is a specification of integer, i,e
   else if (bencodedValue.startsWith('i') && bencodedValue.endsWith('e')) {
     const number = bencodedValue.slice(1, bencodedValue.length - 1);
+    
     if (isNaN(number)) {
       throw new Error("Invalid integer value");
     }
+    
     return parseInt(number);
   }
 
