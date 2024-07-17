e2b18eff443324093af7ec686f681388b635fd0a
s19835 <s19835@sci.pdn.ac.lk>
2024-07-17 19:44:26 +0530

    Decode bencoded lists #AH1

--- app/main.js
+++ app/main.js
@@ -6,105 +6,60 @@ import util from "util";
 // - decodeBencode("10:hello12345") -> "hello12345"
 // - decodeBencode("i52e") -> 52
 // - decodeBencode("l5:helloi52ee") -> ["hello", 52]
+
+// Function to decode bencoded values
 function decodeBencode(bencodedValue) {
-  // Check if the first character is a digit
-  if (!isNaN(bencodedValue[0])) {
-    const firstColonIndex = bencodedValue.indexOf(":");
-    
-    if (firstColonIndex === -1) {
-      throw new Error("Invalid encoded value");
-    }
-    
-    return bencodedValue.substr(firstColonIndex + 1);
-  } 
+  // Function to decode a single bencoded element
+  const decodeElement = (bencodedValue, index) => {
+    let result, newIndex;
 
-  // Check if the first and last character is a specification of integer, i,e
-  else if (bencodedValue.startsWith('i')) {
-    const indexOfe = bencodedValue.indexOf('e');
-
-    const number = indexOfe !== -1 ? bencodedValue.slice(1, indexOfe) : NaN;
-    
-    if (isNaN(number)) {
-      throw new Error("Invalid integer value");
-    }
-    
-    return parseInt(number);
-  }
-
-  // Check for the list condition (starts with l)
-  else if (bencodedValue.startsWith('l')) {
-    const list = [];
-    const endIndex = bencodedValue.length - 1;
-    let i = 1;
-
-    while (i < endIndex) {
-      if (bencodedValue[i] === 'i') {
-        const firstIndexOfE = bencodedValue.indexOf('e', i);
-        const number = bencodedValue.slice(i + 1, firstIndexOfE);
-
-        if (isNaN(number)) {
-          throw new Error('Invalid integer value');
-        } 
-        
-        else {
-          list.push(parseInt(number));
-        }
-
-        i = firstIndexOfE + 1;
-      } 
+    // Decode string
+    if (!isNaN(bencodedValue[index])) {
+      const firstColonIndex = bencodedValue.indexOf(':', index);
       
-      else if (!isNaN(bencodedValue[i])) {
-        let strLength = bencodedValue[i];
-        
-        if (!isNaN(bencodedValue[i + 1])) {
-          strLength += bencodedValue[i + 1];
-        }
-        
-        const firstColonIndex = bencodedValue.indexOf(":", i);
+      if (firstColonIndex === -1) throw new Error("Invalid encoded value");
 
-        if (firstColonIndex === -1) {
-          throw new Error("Invalid encoded value");
-        }
-
-        const str = bencodedValue.substr(firstColonIndex + 1, strLength);
-
-        list.push(str);
-        i = parseInt(strLength) + firstColonIndex + 1;
-      } 
+      const length = parseInt(bencodedValue.slice(index, firstColonIndex));
       
-      else if (bencodedValue[i] === 'l') {
-        let subListStartIndex = i;
-        let bracketCount = 1;
-        i++;
+      newIndex = firstColonIndex + 1 + length;
+      result = bencodedValue.slice(firstColonIndex + 1, newIndex);
+    } 
+    
+    // Decode integer
+    else if (bencodedValue[index] === 'i') {
+      const endOfInt = bencodedValue.indexOf('e', index);
+      
+      if (endOfInt === -1) throw new Error("Invalid integer value");
 
-        while (i < endIndex && bracketCount > 0) {
-          if (bencodedValue[i] === 'l') {
-            bracketCount++;
-          } 
-          
-          else if (bencodedValue[i] === 'e') {
-            bracketCount--;
-          }
-          
-          i++;
-        }
-
-        const sublist = bencodedValue.substring(subListStartIndex, i);
-        list.push(decodeBencode(sublist));
-      } 
+      result = parseInt(bencodedValue.slice(index + 1, endOfInt));
+      newIndex = endOfInt + 1;
+    } 
+    
+    // Decode list
+    else if (bencodedValue[index] === 'l') {
+      let list = [];
+      newIndex = index + 1;
+      
+      while (bencodedValue[newIndex] !== 'e') {
+        const [element, nextIndex] = decodeElement(bencodedValue, newIndex);
+        list.push(element);
         
-      else {
-        i++;
+        newIndex = nextIndex;
       }
+      
+      result = list;
+      newIndex += 1;
+    } 
+    
+    else {
+      throw new Error("Unsupported bencode value");
     }
 
-    return list;
+    return [result, newIndex];
   }
 
-
-  else {
-    throw new Error("Unsupported bencode value");
-  }
+  const [decodedValue] = decodeElement(bencodedValue, 0);
+  return decodedValue;
 }
 
 function main() {
@@ -117,7 +72,9 @@ function main() {
     // In JavaScript, there's no need to manually convert bytes to string for printing
     // because JS doesn't distinguish between bytes and strings in the same way Python does.
     console.log(JSON.stringify(decodeBencode(bencodedValue)));
-  } else {
+  } 
+  
+  else {
     throw new Error(`Unknown command ${command}`);
   }
 }
